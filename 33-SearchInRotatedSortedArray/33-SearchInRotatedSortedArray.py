07c9d3d4e2c0adc2d4a25d3943d469ce580aafee
Rachit1510 <150941826+Rachit1510@users.noreply.github.com>
2025-08-29 18:48:32 +0530

    Create 33-SearchInRotatedSortedArray

---
+++ 33-SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.py
@@ -0,0 +1,22 @@
+# Last updated: 8/29/2025, 6:48:32 PM
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l=0
+        r= len(nums)-1
+        while l<=r:
+            mid = (l+r)//2
+            if target==nums[mid]:
+                return mid
+            
+            #checking left sorted portion
+            if nums[l]<= nums[mid]:
+                if target> nums[mid] or target<nums[l]:
+                    l=mid+1
+                else:
+                    r=mid-1 
+            else:
+                if target<nums[mid] or target>nums[r]:
+                    r=mid-1
+                else:
+                    l=mid+1
+        return -1 
