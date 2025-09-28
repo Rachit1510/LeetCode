903b432811524bd19b886c93a04f430bda257a54
Rachit1510 <150941826+Rachit1510@users.noreply.github.com>
2025-09-28 14:48:27 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -0,0 +1,10 @@
+# Last updated: 9/28/2025, 2:48:27 PM
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        slow=0
+        for fast in range(len(nums)):
+            if nums[fast]!=0 and nums[slow]==0:
+                nums[fast],nums[slow]=nums[slow],nums[fast]
+            
+            if nums[slow]!=0:
+                slow+=1
